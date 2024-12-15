2e3c92b7f293f9a756eddfae72a05376332c1192
Technicfan <107669823+Technicfan@users.noreply.github.com>
2024-12-15 19:25:32 +0100

    add way to import techniques into the model

---
+++ judo_profiles/main/management/commands/import_csv.py
@@ -0,0 +1,27 @@
+import csv
+from django.core.management.base import BaseCommand
+from main.models import Technique
+
+class Command(BaseCommand):
+    help = "Import data from CSV file into Technique model"
+
+    def add_arguments(self, parser):
+        parser.add_argument("csv_file", type=str, help="The CSV file to be imported")
+
+    def handle(self, *args, **options):
+        csv_file = options["csv_file"]
+
+        with open(csv_file, "r") as file:
+            reader = csv.reader(file)
+            next(reader)
+
+            for row in reader:
+                _, created = Technique.objects.get_or_create(
+                    codename=row[0],
+                    name=row[1],
+                    type=row[2]
+                )
+                if created:
+                    self.stdout.write(f"Successfully imported {row[1]}")
+                else:
+                    self.stdout.write(f"Technique {row[1]} already exists")
