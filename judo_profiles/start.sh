6142d0c5d7a4d7f2d64a12e0c1bcf2d5ca899098
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-03-18 19:17:51 +0100

    move to root of repo

--- judo_profiles/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z $(ls "staticfiles") ]]
-then
-    python manage.py collectstatic
-fi
-
-python manage.py migrate
-gunicorn --bind 0.0.0.0:8000 --workers 3 judo_profiles.wsgi:application
