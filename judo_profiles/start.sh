ba7fa46293464f814deba3865d1678cb18076885
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-03-18 19:16:35 +0100

    improve docker

--- judo_profiles/start.sh
+++ judo_profiles/start.sh
@@ -6,4 +6,4 @@ then
 fi
 
 python manage.py migrate
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 judo_profiles.wsgi:application
+gunicorn --bind 0.0.0.0:8000 --workers 3 judo_profiles.wsgi:application
