28a8055b6f1d798ce87de82a8e9151bd37e98c8d
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-05-01 22:32:28 +0200

    test

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ if [[ -z $(ls "staticfiles") ]]
 then
     python manage.py collectstatic
 fi
-if ! [[ -f "nginx_conf/nginx.conf" ]]
+if [[ -f "nginx/nginx.conf" ]]
 then
-    cp nginx/nginx.conf nginx_conf/nginx.conf
+    mv nginx/nginx.conf nginx_conf/default.conf
 fi
 
 python manage.py migrate
