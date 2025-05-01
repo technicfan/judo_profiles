4f612092797c5c9d47f53e25db590e47e69130bf
Technicfan <107669823+Technicfan@users.noreply.github.com>
2025-05-01 21:38:57 +0200

    not a great solution

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@ if [[ -z $(ls "staticfiles") ]]
 then
     python manage.py collectstatic
 fi
+if ! [[ -f "nginx_conf/nginx.conf" ]]
+then
+    cp nginx/nginx.conf nginx_conf/nginx.conf
+fi
 
 python manage.py migrate
 gunicorn --bind 0.0.0.0:8000 --workers 3 judo_profiles.wsgi:application
